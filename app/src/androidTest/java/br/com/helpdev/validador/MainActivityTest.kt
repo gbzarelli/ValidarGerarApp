552466facf525c24cb4e3057b0b65a77e8943041
Guilherme Biff Zarelli <guilhermeb@grupocriar>
2018-12-28 13:22:04 -0200

    Include android test

--- app/src/androidTest/java/br/com/helpdev/validador/MainActivityTest.kt
+++ app/src/androidTest/java/br/com/helpdev/validador/MainActivityTest.kt
@@ -2,14 +2,19 @@ package br.com.helpdev.validador
 
 
 import android.support.test.InstrumentationRegistry
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.*
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import org.hamcrest.Matchers.allOf
+import br.com.helpdev.libvalidadores.CNPJ
+import br.com.helpdev.libvalidadores.CPF
+import br.com.helpdev.libvalidadores.PIS
+import br.com.helpdev.libvalidadores.RENAVAM
+import br.com.helpdev.validador.EspressoUtils.inputTextAndCloseKeyboard
+import br.com.helpdev.validador.EspressoUtils.performClickAndCheckStringOfView
+import br.com.helpdev.validador.EspressoUtils.checkIsDisplayedViewFromId
+import br.com.helpdev.validador.EspressoUtils.getTextFromResId
+import br.com.helpdev.validador.EspressoUtils.matchesText
+import br.com.helpdev.validador.EspressoUtils.performClickSpinnerAndCheckValue
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
@@ -36,33 +41,57 @@ class MainActivityTest {
     }
 
     @Test
-    fun shouldDisplayDefault() {
-        onView(withId(R.id.layout_validar))
-                .check(matches(isDisplayed()))
+    fun app_ShouldDisplay_DefaultScreen_Successfully() {
+        checkIsDisplayedViewFromId(R.id.layout_validar)
     }
 
     @Test
-    fun cpfMustBeValidatedSuccessfully() {
-        val editText = onView(allOf(withId(R.id.edit_valor_doc), isDisplayed()))
-        editText.perform(replaceText(VALID_CPF), closeSoftKeyboard())
-
-        val btValidate = onView(allOf(withId(R.id.bt_validar), isDisplayed()))
-        btValidate.perform(click())
-
-        val tvResult = onView(allOf(withId(R.id.tv_resultado_validar), isDisplayed()))
-        tvResult.check(matches(withText(R.string.valor_valido)))
+    fun cpf_MustBe_Validated_Successfully() {
+        inputTextAndCloseKeyboard(R.id.edit_valor_doc, VALID_CPF)
+        performClickAndCheckStringOfView(R.id.bt_validar, R.string.validar)
+        matchesText(R.id.tv_resultado_validar, R.string.valor_valido)
     }
 
     @Test
-    fun cpfMustBeValidatedFailed() {
-        val editText = onView(allOf(withId(R.id.edit_valor_doc), isDisplayed()))
-        editText.perform(replaceText(INVALID_CPF), closeSoftKeyboard())
+    fun cpf_MustBe_Validated_Failed() {
+        inputTextAndCloseKeyboard(R.id.edit_valor_doc, INVALID_CPF)
+        performClickAndCheckStringOfView(R.id.bt_validar, R.string.validar)
+        matchesText(R.id.tv_resultado_validar, R.string.valor_invalido)
+    }
 
-        val btValidate = onView(allOf(withId(R.id.bt_validar), isDisplayed()))
-        btValidate.perform(click())
+    @Test
+    fun cnpj_MustBe_GeneratedAndValidated_Successfully() {
+        performClickSpinnerAndCheckValue(R.id.sp_docs, 1, R.string.cnpj)
+        performGenerateAndCheckLayout()
+        assert(CNPJ.validateCNPJ(getTextFromResId(R.id.tv_resultado_gerar)))
+    }
 
-        val tvResult = onView(allOf(withId(R.id.tv_resultado_validar), isDisplayed()))
-        tvResult.check(matches(withText(R.string.valor_invalido)))
+    @Test
+    fun cpf_MustBe_GeneratedAndValidated_Successfully() {
+        performClickSpinnerAndCheckValue(R.id.sp_docs, 0, R.string.cpf)
+        performGenerateAndCheckLayout()
+        assert(CPF.validateCPF(getTextFromResId(R.id.tv_resultado_gerar)))
+    }
+
+    @Test
+    fun renavan_MustBe_GeneratedAndValidated_Successfully() {
+        performClickSpinnerAndCheckValue(R.id.sp_docs, 4, R.string.renavan)
+        performGenerateAndCheckLayout()
+        assert(RENAVAM.validateRENAVAM(getTextFromResId(R.id.tv_resultado_gerar)))
+    }
+
+    @Test
+    fun pis_MustBe_GeneratedAndValidated_Successfully() {
+        performClickSpinnerAndCheckValue(R.id.sp_docs, 2, R.string.pis)
+        performGenerateAndCheckLayout()
+        assert(PIS.validatePIS(getTextFromResId(R.id.tv_resultado_gerar)))
+    }
+
+
+    private fun performGenerateAndCheckLayout() {
+        performClickAndCheckStringOfView(R.id.rb_gerar, R.string.gerar)
+        performClickAndCheckStringOfView(R.id.bt_gerar, R.string.gerar)
+        checkIsDisplayedViewFromId(R.id.layout_valor_gerado)
     }
 
 }
